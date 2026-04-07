5b387d05ac53b3517609a7b97b335afdaa83dd31
Ekira <123889544+ekira-muriithi@users.noreply.github.com>
2026-04-07 09:28:27 +0300

    Obscure sensitive login information in test script
    Updated email and password fields to use placeholders.

--- .automation/playwright/Hampton-Test-Scripts/login.spec.js
+++ .automation/playwright/Hampton-Test-Scripts/login.spec.js
@@ -5,8 +5,8 @@ test('login', async({page}) => {
     await page.goto(url);
     await expect(page.locator("//button[normalize-space()='Login']")).toBeVisible();
     await page.click("//button[normalize-space()='Login']");
-    await page.fill("#email","murugiekra1@gmail.com");
-    await page.fill("#password","12345678");
+    await page.fill("#email","*******@gmail.com");
+    await page.fill("#password","********");
     await page.click("//button[normalize-space()='Sign In']");
     await expect(page.locator("//button[normalize-space()='My Profile']")).toBeVisible();
 });
